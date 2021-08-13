f2904156d63e692906351733dba3a9b8c1e8a0fa
keijop <76419484+keijop@users.noreply.github.com>
2021-08-13 14:14:23 +0300

    Update server.js

--- server.js
+++ server.js
@@ -14,5 +14,5 @@ app.get('/', (req, res) => {
 
 
 const listener = app.listen(process.env.PORT || 3000, () => {
-  console.log('Your app is listening on port ' + listener.address().port)
+  console.log(`Your app is listening on port ${listener.address().port}...`)
 })
