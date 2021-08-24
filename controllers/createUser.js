99a8bf02ca1c9a3c9f9e54c708d1ea5f8fe8b2f3
keijop <76419484+keijop@users.noreply.github.com>
2021-08-24 10:42:19 +0300

    Update createUser.js

--- controllers/createUser.js
+++ controllers/createUser.js
@@ -5,6 +5,11 @@ const createUser = async (req,res) => {
 	const {username} = req.body
   	console.log('user hit server')
   	try {
+  		const user = await User.findOne({username : username})
+  		console.log(user)
+  		if(user){
+  			return res.status(200).json({error : 'Username already in use'})
+  		}
 	    const newUser = await User.create({username : username})
 	    res.status(201).json({username : newUser.username, _id : newUser._id})
  	} catch (err) {
